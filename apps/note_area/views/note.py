68b9ef6773fbc433e448994d54ba2fcae0a49af2
77CBY <bo77workspace@gmail.com>
2024-03-20 17:04:29 +0800

    筆記區學習影片區快好了

--- apps/note_area/views/note.py
+++ apps/note_area/views/note.py
@@ -1,4 +1,5 @@
 import os
+import random
 from django.http import JsonResponse
 from django.shortcuts import render, redirect, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -26,20 +27,72 @@ def note(request):
     
     if request.POST['purpose'] == 'showMore':
         if request.POST['id'] == 'learningAreaCourseMore': 
-            # 要獲取課名
-            full_file_name_list = [file for file in os.listdir(USER_COMPUTER_PATH) if not file.endswith('.txt')] # 這個路徑中的readMe如果都知道了就可以把該檔案刪掉, 也就可以不用判斷式而直接用listdir就好(可以省很多效能)
-            course_list = list(set([file_name.split('[]')[1] for file_name in full_file_name_list])) # 每一個元素都不重複, 所以做這些轉換
+            # 要獲取學習影片區課名
+            full_folder_name_list = os.listdir(USER_COMPUTER_PATH)
+            course_list = list(set([file_name.split('[]')[1] for file_name in full_folder_name_list])) # 每一個元素都不重複, 所以做這些轉換
             context = {
                 'status': True,
                 'data': course_list,
             }
             return JsonResponse(context)
         elif request.POST['id'] == 'learningAreaLecturerMore':
-            # 要獲取授課者
-            full_file_name_list = [file for file in os.listdir(USER_COMPUTER_PATH) if not file.endswith('.txt')] # 這個路徑中的readMe如果都知道了就可以把該檔案刪掉, 也就可以不用判斷式而直接用listdir就好(可以省很多效能)
-            lecturer_list = list(set([file_name.split('[]')[0] for file_name in full_file_name_list])) # 每一個元素都不重複, 所以做這些轉換
+            # 要獲取學習影片區授課者
+            full_folder_name_list = os.listdir(USER_COMPUTER_PATH)
+            lecturer_list = list(set([file_name.split('[]')[0] for file_name in full_folder_name_list])) # 每一個元素都不重複, 所以做這些轉換
             context = {
                 'status': True,
                 'data': lecturer_list,
             }
             return JsonResponse(context)
+        else:
+            # 習題答案區的按鈕
+            pass
+        
+    if request.POST['purpose'] == 'mainPageBtn':
+        # 學習影片區
+        full_folder_name_list = os.listdir(USER_COMPUTER_PATH)
+        choices = random.sample(full_folder_name_list, 2)
+        course_list = list(set([file_name.split('[]')[1] for file_name in choices]))
+        lecturer_list = list(set([file_name.split('[]')[0] for file_name in choices])) 
+        
+        # 習題答案區
+        
+        context = {
+            'status': True,
+            'data': {
+                'course_list': course_list,
+                'lecturer_list': lecturer_list,
+            }
+        }
+        return JsonResponse(context)
+    
+    if request.POST['purpose'] == 'renameLearning':
+        old_name = request.POST.get('oldName', '')
+        new_name = request.POST.get('newName', '')
+        full_folder_name_list = os.listdir(USER_COMPUTER_PATH)
+        for folder_name in full_folder_name_list:
+            if old_name in folder_name:
+                old_path = os.path.join(USER_COMPUTER_PATH, folder_name)
+                new_folder_name = folder_name.replace(old_name, new_name, 1)
+                new_path = os.path.join(USER_COMPUTER_PATH, new_folder_name)
+                os.rename(old_path, new_path)
+
+        context = {
+            'status': True,
+            'data': '重新命名成功!',
+        }
+        return JsonResponse(context)
+    
+    if request.POST['purpose'] == 'showLearningAreaData':
+        filter_text = request.POST.get('filterText', '')
+        full_folder_name_list = os.listdir(USER_COMPUTER_PATH)
+        data_list = []
+        for folder_name in full_folder_name_list:
+            if filter_text in folder_name:
+                data_list.append(folder_name.split('[]'))
+
+        context = {
+            'status': True,
+            'data': data_list,
+        }
+        return JsonResponse(context)
