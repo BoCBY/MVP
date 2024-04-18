70ad4c51664e43c0afc0f0a3799d18abd5393b68
77CBY <bo77workspace@gmail.com>
2024-04-18 17:02:36 +0800

    所有筆記板: 標籤生成, 學習區複製連結, 學習區前往學習區

--- apps/all_notes_area/views/all_notes.py
+++ apps/all_notes_area/views/all_notes.py
@@ -2,6 +2,7 @@ import os
 from django.http import JsonResponse
 from django.shortcuts import render, redirect, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from apps.all_notes_area.utils import cleaned_area_data
 
 COMPUTER_DESK = os.path.join('C:\\', 'Users', 'admin', 'Desktop')
 NOTEBOOK_DESK = os.path.join('C:\\', 'Users', 'jerry', 'OneDrive', '桌面')
@@ -14,6 +15,31 @@ NOTEBOOK_USER_INFO_PATH = os.path.join(NOTEBOOK_DESK, USER_INFO)
 SELF_DEFINED_PATH = os.path.join('email#1', 'note', 'self_defined')
 SELF_DEFINED_AREA_PATH = os.path.join(COMPUTER_USER_INFO_PATH, SELF_DEFINED_PATH)
 
+@csrf_exempt
 def all_notes(request):
     if request.method == 'GET':
         return render(request, 'all_notes.html')
+    if request.POST['purpose'] == 'selectLearningArea':
+        video_list = cleaned_area_data.clean_learning()
+        context = {
+            'status': True,
+            'data': video_list,
+            }
+        return JsonResponse(context)
+    
+    if request.POST['purpose'] == 'selectExerciseAnswerArea':
+        subject_list = cleaned_area_data.clean_exercise_answer()
+        context = {
+            'status': True,
+            'data': subject_list,
+            }
+        return JsonResponse(context)
+    
+    if request.POST['purpose'] == 'selectSelfDefinedArea':
+        location_list = cleaned_area_data.clean_self_defined()
+        context = {
+            'status': True,
+            'data': location_list,
+            }
+        return JsonResponse(context)
+    
